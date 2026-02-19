da3cadeebc77abe0c8d1894cbd3e8c7f8ebdb6b4
Ping-Varun001 <varunpingle9@gmail.com>
2026-02-20 00:25:44 +0530

    Enable auto migrate and admin creation

--- exam/apps.py
+++ exam/apps.py
@@ -3,3 +3,19 @@ from django.apps import AppConfig
 
 class examConfig(AppConfig):
     name = 'exam'
+
+from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+import os
+
+class ExamConfig(AppConfig):
+    name = 'exam'
+
+    def ready(self):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email=os.environ.get('EMAIL_HOST_USER'),
+                password=os.environ.get('ADMIN_PASSWORD')
+            )
