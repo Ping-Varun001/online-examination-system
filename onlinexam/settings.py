b4a31012483635b2cc6274aec8bd91760c3f310f
Ping-Varun001 <varunpingle9@gmail.com>
2026-02-20 00:47:07 +0530

    Force PostgreSQL timezone UTC for Django 3 on Render

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -106,9 +106,6 @@ if DATABASE_URL:
         conn_max_age=600,
         ssl_require=True
     )
-    DATABASES['default']['OPTIONS'] = {
-        'options': '-c timezone=UTC'
-    }
 
 # -----------------------------
 # PASSWORD VALIDATION
@@ -164,3 +161,12 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_RECEIVING_USER = [
     os.environ.get('EMAIL_RECEIVING_USER', EMAIL_HOST_USER)
 ]
+
+# -----------------------------
+# Timezone for PostgreSQL
+# -----------------------------
+
+from django.db.backends.postgresql.base import DatabaseWrapper
+
+DatabaseWrapper.timezone = 'UTC'
+DatabaseWrapper.timezone_name = 'UTC'
