104b30e725cd622b8c3f9c4e56db22d00b11efb8
Ping-Varun001 <varunpingle9@gmail.com>
2026-02-20 22:09:33 +0530

    Created new app for super user login

--- exam/apps.py
+++ exam/apps.py
@@ -1,21 +1,30 @@
 from django.apps import AppConfig
-
-
-class examConfig(AppConfig):
-    name = 'exam'
-
-from django.apps import AppConfig
 from django.contrib.auth import get_user_model
 import os
+from django.db.utils import OperationalError
 
 class ExamConfig(AppConfig):
     name = 'exam'
 
     def ready(self):
-        User = get_user_model()
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='admin',
-                email=os.environ.get('EMAIL_HOST_USER'),
-                password=os.environ.get('ADMIN_PASSWORD')
-            )
+        try:
+            User = get_user_model()
+
+            username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+            email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+            password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+            if not username or not password:
+                return  # env vars not set
+
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(
+                    username=username,
+                    email=email,
+                    password=password
+                )
+                print("✅ Superuser created automatically")
+
+        except OperationalError:
+            # DB not ready during migrate
+            pass
