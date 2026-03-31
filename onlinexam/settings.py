91a3b147a83169d4b5211cdadc988ed48fb6e132
Ping-Varun001 <varunpingle9@gmail.com>
2026-03-31 17:13:29 +0530

    Render Database Issue Solved

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -95,15 +95,19 @@ WSGI_APPLICATION = 'onlinexam.wsgi.application'
 # -----------------------------
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if DATABASE_URL:
+if os.getenv('DATABASE_URL'):
     DATABASES = {
-        "default": dj_database_url.parse(DATABASE_URL)
+        'default': dj_database_url.parse(
+            os.getenv('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
 else:
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 # -----------------------------
